a1f4dcef6a60ba81da7eaa367365569cbbe8bda5
BadGuyIvan <badguy.ivan.i@gmail.com>
2018-10-28 12:06:32 +0200

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import style from "./App.scss";
+
+class App extends Component {
+  render() {
+    return <div className={style.App}>Hello React</div>;
+  }
+}
+
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,51 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+              localIdentName: "[name]__[local]--[hash:base64:5]"
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
